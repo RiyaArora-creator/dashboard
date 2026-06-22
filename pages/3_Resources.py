a6b79bd41a479ace8cbb0284dc639418103e2cae
RiyaArora-creator <riyaarora2049@gmail.com>
2026-06-22 18:05:23 +0530

    Create 3_Resources.py

---
+++ pages/3_Resources.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Resources")
+
+st.write("Industry insights and best practices")
+
+st.markdown("""
+- Workforce Trends
+- Productivity Insights
+- Digital Transformation
+- Future of Work
+""")
